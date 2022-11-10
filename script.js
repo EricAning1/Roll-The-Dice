4a5542afd260bdb2eea13f349e7ae668f8ec6fde
EricAning1 <ericaning@hotmail.com>
2022-11-10 13:22:33 -0600

    js changes

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//
+const score1 = document.querySelector('.score1');
+const score2 = document.querySelector('.score2');
+console.log(score1.textContent);
+const display1 = document.querySelector('.display1');
+const display2 = document.querySelector('.display2');
+console.log(display1.textContent);
+const dice = document.querySelector('.dice');
