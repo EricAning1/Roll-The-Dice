cecd30d2a0a892dc4e4d833fdea8f6eea6f66889
EricAning1 <ericaning@hotmail.com>
2022-11-10 14:37:06 -0600

    updates

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const display1 = document.querySelector('.display1');
 const display2 = document.querySelector('.display2');
 console.log(display1.textContent);
 const dice = document.querySelector('.dice');
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+const newGame = document.querySelector('.refresh');
+const roll = document.querySelector('.roll');
+console.log(roll.textContent);
+dice.classList.add('hidden');
+
+roll.addEventListener('click', function () {
+  const diceRoll = Math.floor(Math.random() * 6) + 1;
+  dice.classList.remove('hidden');
+  roll.style.cursor = 'pointer';
+  newGame.style.cursor = 'pointer';
+  dice.src = `image/dice-${diceRoll}.png`;
+  console.log(diceRoll);
+  display1.textContent = diceRoll;
+});
